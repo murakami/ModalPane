5d982b81d6a77c74723181d536919b7e2a6106c0
村上 幸雄 <yukio@mbc2d.local>
2012-01-19 21:54:58 +0900

    Initial Commit

---
+++ ModalPaneTests/ModalPaneTests.h
@@ -0,0 +1,13 @@
+//
+//  ModalPaneTests.h
+//  ModalPaneTests
+//
+//  Created by 村上 幸雄 on 12/01/19.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ModalPaneTests : SenTestCase
+
+@end
---
+++ ModalPaneTests/ModalPaneTests.m
@@ -0,0 +1,32 @@
+//
+//  ModalPaneTests.m
+//  ModalPaneTests
+//
+//  Created by 村上 幸雄 on 12/01/19.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "ModalPaneTests.h"
+
+@implementation ModalPaneTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ModalPaneTests");
+}
+
+@end
